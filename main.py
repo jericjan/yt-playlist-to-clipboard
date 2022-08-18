b12bae66327681fdcfefaba27567f4e3e174a27f
jericjan <cantilfrederick@gmail.com>
2022-08-18 12:51:18 +0800

    merged manual_main and main

--- main.py
+++ main.py
@@ -1,97 +1,141 @@
-import pyperclip
-from pynput.keyboard import Key, Listener, Controller
+"""
+The Main File
+"""
+
 from subprocess import Popen, PIPE
+import os
 import json
+import sys
+import argparse
+from pynput.keyboard import Key, Listener, Controller
+import pyperclip
 
-print('Enter your YT playlist:')
-url = input()
-print("Enter prefix: (blank to skip)")
-prefix = input()
-print("\nPlease wait...\n")
-process = Popen(['youtube-dl', '--dump-json', '--flat-playlist',url], stdout=PIPE, stderr=PIPE, stdin=PIPE)
-stdout, stderr = process.communicate()
-str_output = stdout.decode("utf-8")
-str_output = str_output.splitlines()
-str_output = ','.join(str_output)
-str_output = f"[{str_output}]"
-json_output = json.loads(str_output)
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--manual", action="store_true")
+args = vars(parser.parse_args())
 
 
-title_list = []    
-url_list = []
-for i in json_output:
-    if "youtube" in url:
-        x = i['title']
-        title_list.append(x)
-        x = i['id']
-        x = prefix + "https://youtu.be/"+x
-        url_list.append(x)
-    elif "soundcloud" in url:
-        x = i['title']
-        title_list.append(x)
-        x = prefix + i['url']
-        url_list.append(x)        
-title_list.reverse()
-url_list.reverse()    
+START, END = 0, -1
+EXTRA = ""
 
-for idx, i in enumerate(title_list):
-    print(f"{idx} - {i}")
-
-def get_start():    
-    global start
+def get_start():
+    "Asks for the index to start at"
+    global START
     print("\nType what number you want start at:")
-    start = input()
-    start = int(start)
+    START = input()
+    START = int(START)
     try:
-        url_list[start]
-    except:
+        url_list[START]
+    except IndexError:
         print("Index doesn't exist. Try again.")
         get_start()
-   
+
+
 def get_end():
-    global end
+    "Asks for the index to end at"
+    global END
     print("\nType what number you want to end at:")
-    end = input()   
-    end = int(end)
+    END = input()
+    END = int(END)
     try:
-        url_list[end]
-    except:
+        url_list[END]
+    except IndexError:
         print("Index doesn't exist. Try again.")
-        get_end()        
-    end = int(end) + 1
+        get_end()
+    END = int(END) + 1
 
-    
-get_start()
-get_end()    
-selected_list = url_list[start:end]
 
-# for i in selected_list:
-    # print(i)
-    
-with open('URLs.txt', 'w') as f:
-    f.write('\n'.join(selected_list))    
+print("Enter prefix: (blank to skip)")
+prefix = input()
 
-print("\nNow press the right arrow key and it will paste the URLs in order on every press.\nIt is also inside URLs.txt in case you want to use those URLs for something else.")
-    
+if args["manual"]:
+    if not os.path.exists("manual.txt"):
+        print(
+            "There is no manual.txt file. Creating one now.\n"
+            "Place your urls in here separeted by newlines"
+        )
+        with open("manual.txt", "w", encoding="utf-8") as f:
+            f.write("")
+        sys.exit()
+
+    with open("manual.txt", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    if not lines:
+        print("manual.txt is empty. put stuff in it")
+        sys.exit()
+    else:
+        selected_list = [f"{prefix}{x.strip()}" for x in lines]
+else:
+    print("Enter your YT playlist:")
+    url = input()
+    print("\nPlease wait...\n")
+    with Popen(
+        ["youtube-dl", "--dump-json", "--flat-playlist", url],
+        stdout=PIPE,
+        stderr=PIPE,
+        stdin=PIPE,
+    ) as process:
+        stdout, stderr = process.communicate()
+    STR_OUTPUT = stdout.decode("utf-8")
+    STR_OUTPUT = STR_OUTPUT.splitlines()
+    STR_OUTPUT = ",".join(STR_OUTPUT)
+    STR_OUTPUT = f"[{STR_OUTPUT}]"
+    json_output = json.loads(STR_OUTPUT)
+
+    title_list = []
+    url_list = []
+    for i in json_output:
+        if "youtube" in url:
+            x = i["title"]
+            title_list.append(x)
+            x = i["id"]
+            x = prefix + "https://youtu.be/" + x
+            url_list.append(x)
+        elif "soundcloud" in url:
+            x = i["title"]
+            title_list.append(x)
+            x = prefix + i["url"]
+            url_list.append(x)
+    title_list.reverse()
+    url_list.reverse()
+
+    for idx, i in enumerate(title_list):
+        print(f"{idx} - {i}")
+
+    get_start()
+    get_end()
+    selected_list = url_list[START:END]
+    with open("URLs.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(selected_list))
+    EXTRA = "It is also inside URLs.txt in case you want to use those URLs for something else."
+
+print(
+    "\nNow press the right arrow key and it will paste the URLs in order on every press."
+    f"\n{EXTRA}"
+)
+
 # with open('URLs.txt') as f:
-    # lines = f.readlines()
+# lines = f.readlines()
 
-index = 0
+INDEX = 0
+
 
 def on_release(key):
-    global index
-    if key == Key.right:        
+    "Check for a released key, and if it is the Right key, paste text"
+    global INDEX
+    if key == Key.right:
         keyboard = Controller()
         try:
-            pyperclip.copy(selected_list[index])
+            pyperclip.copy(selected_list[INDEX])
             keyboard.press(Key.ctrl)
-            keyboard.press('v')
+            keyboard.press("v")
             keyboard.release(Key.ctrl)
-            keyboard.release('v')        
-            index += 1
+            keyboard.release("v")
+            INDEX += 1
         except IndexError:
             pass
 
+
 with Listener(on_release=on_release) as listener:
     listener.join()
-    
